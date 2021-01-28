cdf3b85bfde5975665fe8e36cbfee4b402788d66
Jaco Greeff <jacogr@gmail.com>
2021-01-28 09:36:12 +0100

    Map ids

--- index.mjs
+++ index.mjs
@@ -17,12 +17,15 @@ async function query () {
 
   while (true) {
     const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
-    const [v, d] = await Promise.all([
+    const [v, _d] = await Promise.all([
       // Vec<AccountId>
       api.query.session.validators.at(blockHash),
       // Vec<u32>
       api.query.session.disabledValidators.at(blockHash)
     ]);
+
+    // map disabled indexes to validatorId
+    const d = _d.map((i) => v[i.toNumber()]);
 
     console.log(new Date(), blockNumber.toString().padStart(9), blockHash.toHex(), getCount(v), getCount(d));
 
